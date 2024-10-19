30f7f5fcdadb690f94edb12ba6b47fd23e92c022
Deepak9551 <12pcm.deepak@gmail.com>
2024-10-19 23:39:49 +0530

    first commit

--- firstboot/src/main/java/work/Entity/Work.java
+++ firstboot/src/main/java/work/Entity/Work.java
@@ -1,21 +1,21 @@
-//package work.Entity;
-//
-//
-//import com.boot.firstboot.FirstbootApplication;
-//import com.boot.firstboot.entity.Student;
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cglib.core.Local;
-//import org.springframework.context.ApplicationContext;
-//import org.springframework.context.annotation.PropertySource;
-//
-//@SpringBootApplication
-//@PropertySource("student.yml")
-//public class Work {
-//    public static void main(String[] args) {
-//        ApplicationContext context  = null;
-//        context =	SpringApplication.run(FirstbootApplication.class, args);
-//     Student student =  context.getBean("student", Student.class);
-//               System.out.println(student);
-//    }
-//}
+package work.Entity;
+
+
+import com.boot.firstboot.FirstbootApplication;
+import com.boot.firstboot.entity.Student;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cglib.core.Local;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@PropertySource("student.yml")
+public class Work {
+    public static void main(String[] args) {
+        ApplicationContext context  = null;
+        context =	SpringApplication.run(FirstbootApplication.class, args);
+     Student student =  context.getBean("student", Student.class);
+               System.out.println(student);
+    }
+}
